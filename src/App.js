33f10c593e0a8a433b6eebc543e4752c5ca59eac
kevinstaresdarbon <red.inshou@gmail.com>
2025-04-27 04:10:09 +0000

    refactors the app.js to use react-router-dom and adds a few dummy pages for testing

--- src/App.js
+++ src/App.js
@@ -1,31 +1,40 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import logo from './logo.png';
 import Search from './components/Search/Search';
+import HomePage from './components/HomePage/HomePage';
+import Dashboard from './components/Dashboard/Dashboard';
 import './App.css';
 
 function App() {
 
-  async function handleSearchSubmit(){
+  async function handleSearchSubmit() {
 
   }
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <figure className='App-logo-container'>
-          <img src={logo} className="App-logo" alt="logo: A spinning star" />
-        </figure>
-        <Search />
-        <nav>
-          <ul>
-            <li><a href='#'>Login</a></li>
-            <li><a href='#'>Sign Up</a></li>
-          </ul>
-        </nav>
-      </header>
-      <main>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <figure className='App-logo-container'>
+            <img src={logo} className="App-logo" alt="logo: A spinning star" />
+          </figure>
+          <Search />
+          <nav>
+            <ul>
+              <li><a href='#'>Login with Spotify</a></li>
+            </ul>
+          </nav>
+        </header>
+        <main>
+          <Switch>
+            <Route exact path='/' component={HomePage} />
+            <Route path='/dashboard' component={Dashboard} />
+          </Switch>
+        </main>
+      </div>
+    </Router>
 
-      </main>
-    </div>
   );
 }
 
---
+++ src/components/HomePage/HomePage.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+function HomePage() {
+
+    return <h1>This is the HomePage!!!</h1>;
+}
+
+export default HomePage
